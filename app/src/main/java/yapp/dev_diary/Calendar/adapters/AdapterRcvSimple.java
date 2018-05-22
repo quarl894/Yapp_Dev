77d9245a17610d2dce5bdd909910a42baa0a59e0
YoungK <quarl894@gmail.com>
2018-05-22 15:08:44 +0900

    디자인 및 버그 일부 수정

--- app/src/main/java/yapp/dev_diary/Calendar/adapters/AdapterRcvSimple.java
+++ app/src/main/java/yapp/dev_diary/Calendar/adapters/AdapterRcvSimple.java
@@ -1,5 +1,6 @@
 package yapp.dev_diary.Calendar.adapters;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
